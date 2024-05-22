c41f2c879488372d1cb3ef896eac93b6de1fe1f3
Artur Maslov <aartasas@gmail.com>
2024-05-22 13:38:17 +0300

    initial widget

--- app/src/main/java/com/arturmaslov/agery/AppWidget.kt
+++ app/src/main/java/com/arturmaslov/agery/AppWidget.kt
@@ -1,24 +1,32 @@
 package com.arturmaslov.agery
 
 import android.content.Context
-import androidx.compose.foundation.layout.Column
-import androidx.compose.material3.Text
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.ui.unit.dp
+import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.intPreferencesKey
 import androidx.datastore.preferences.core.stringPreferencesKey
 import androidx.glance.GlanceId
+import androidx.glance.GlanceModifier
+import androidx.glance.GlanceTheme
 import androidx.glance.appwidget.GlanceAppWidget
 import androidx.glance.appwidget.GlanceAppWidgetReceiver
 import androidx.glance.appwidget.provideContent
 import androidx.glance.appwidget.state.updateAppWidgetState
 import androidx.glance.appwidget.updateAll
 import androidx.glance.currentState
+import androidx.glance.layout.Alignment
+import androidx.glance.layout.Column
+import androidx.glance.layout.Spacer
+import androidx.glance.layout.height
+import androidx.glance.layout.padding
 import androidx.glance.state.PreferencesGlanceStateDefinition
+import androidx.glance.text.Text
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.runBlocking
 import java.util.Calendar
 import java.util.concurrent.TimeUnit
-import java.util.prefs.Preferences
 
 class AgeWidget : GlanceAppWidget() {
     override suspend fun provideGlance(context: Context, id: GlanceId) {
@@ -36,13 +44,13 @@ class AgeWidget : GlanceAppWidget() {
         val currentDate = Calendar.getInstance()
         val diff = currentDate.timeInMillis - birthDate.timeInMillis
         val diffDays = TimeUnit.MILLISECONDS.toDays(diff)
-        val diffHours = TimeUnit.MILLISECONDS.toHours(diff) % 24
-        val diffMinutes = TimeUnit.MILLISECONDS.toMinutes(diff) % 60
+        val diffHours = diffDays * 24
+        val diffMinutes = diffHours * 60
 
         updateAppWidgetState(context, PreferencesGlanceStateDefinition, id) { prefs ->
             prefs.toMutablePreferences().apply {
                 this[stringPreferencesKey("ageText")] =
-                    "Age: $diffDays days, $diffHours hours, $diffMinutes minutes"
+                    "$diffDays days, $diffHours hours, $diffMinutes minutes"
             }
         }
 
@@ -50,10 +58,26 @@ class AgeWidget : GlanceAppWidget() {
 
         provideContent {
             val prefs = currentState<Preferences>()
-            val ageText = prefs[stringPreferencesKey("ageText").toString(), ""] ?: "Age: -"
+            val ageText = prefs[stringPreferencesKey("ageText")] ?: "Age: -"
 
-            Column {
-                Text(text = ageText)
+            GlanceTheme {
+                MaterialTheme {
+                    Column(
+                        modifier = GlanceModifier.padding(16.dp),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        val ageParts = ageText.split(", ")
+                        Column {
+                            ageParts.forEach { part ->
+                                Text(
+                                    text = part
+                                )
+                                Spacer(modifier = GlanceModifier.height(4.dp))
+                            }
+                        }
+                    }
+                }
             }
         }
     }
